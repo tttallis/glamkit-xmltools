1062a263e537b6bb5da7f0e734b553da0ab02c22
Thomas Ashelford <thomas@ether.com.au>
2013-02-12 11:50:05 +1100

    replaced generator with good old list, so we can check for empty directories, file sizes etc.

--- xmltools/lib/getfiles.py
+++ xmltools/lib/getfiles.py
@@ -3,6 +3,8 @@ import re
 
 def getfiles(paths, regex=r"", recursive=True, followlinks=True):
     """generates a list of file paths of files in the given paths that match a given regex"""
+    
+    files = []
 
     for path in paths:
         rex = re.compile(regex)
@@ -10,17 +12,19 @@ def getfiles(paths, regex=r"", recursive=True, followlinks=True):
         if os.path.isfile(path):
             p = os.path.abspath(path)
             if rex.search(p):
-                yield p
+                files.append(p)
         else:
             if recursive:
                 for root, dirs, files in os.walk(path, followlinks):
                     for f in files:
                         p = os.path.abspath(os.path.join(root, f))
                         if rex.search(p):
-                            yield p
+                            files.append(p)
             else:
                 for f in os.listdir(path):
                     p = os.path.abspath(os.path.join(path, f))
                     if os.path.isfile(p):
                         if rex.search(p):
-                            yield p
+                            files.append(p)
+                            
+        return files
